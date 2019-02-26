973e4f26e9f6c6a98a5c62721fb6e11bbb093ec9
shweta <sdudi@sapient.com>
2019-02-26 18:18:28 +0530

    select all removed

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -51,7 +51,7 @@ export class RegisterComponent implements OnInit {
             { item_id: 2, item_text: 'House Cleaning' },
             { item_id: 3, item_text: 'Dish Washing' },
             { item_id: 4, item_text: 'Car Cleaning' },
-            { item_id: 5, item_text: 'Car Cleaning' },
+            { item_id: 5, item_text: 'All rounder' },
           ];
           this.selectedItems = [
             
@@ -72,6 +72,7 @@ export class RegisterComponent implements OnInit {
             selectAllText: 'Select All',
             unSelectAllText: 'UnSelect All',
             itemsShowLimit: 3,
+            enableCheckAll : false,
             allowSearchFilter: true
           };
           this.ServicesLocSettings = {
@@ -81,6 +82,7 @@ export class RegisterComponent implements OnInit {
             selectAllText: 'Select All',
             unSelectAllText: 'UnSelect All',
             itemsShowLimit: 3,
+            enableCheckAll : false,
             allowSearchFilter: true
           };
     }
